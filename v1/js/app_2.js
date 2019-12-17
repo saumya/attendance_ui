fe9edc4398c8e891584beadc7a255577cbec9816
saumya <2saumyaray@gmail.com>
2019-12-17 13:16:20 +0530

    UX fixing

--- v1/js/app_2.js
+++ v1/js/app_2.js
@@ -6,6 +6,8 @@ $(function(){
 
 	var requestURL_getBatchNames = REST_URI+'/getBatchNames/';
 	var requestURL_addPerson = REST_URI+'/addPerson/';
+	//
+	var addButtonIsDisabled = false;
 	//---------------------------------------------------------------------
 	$('#div_info').hide();
 	$('#div_notification').hide();
@@ -70,24 +72,19 @@ $(function(){
 		}
 	});
 	//---------------------------------------------------------------------
+	/*
 	// Get Batch names to show
 	$('#btn_show_allBatchNames').on('click',function(event){
 		console.log('getAllBatchNames');
 		event.preventDefault();
 		const msg = 'Info: Please wait, getting data.';
 		showInfoToUser(msg);
-
 		//
 		fetch(requestURL_getBatchNames).then(function(resultData){
 			console.log('getAllBatchNames: done:');
 			resultData.json()
 		  	.then(function(rData){
 		  		$('#div_info').hide();
-		  		/*
-		  		console.log('------- data');
-		  		console.log(rData);
-		  		console.log('------- data /');
-		  		*/
 		  		renderAllBatchNames(rData);
 		  	})
 		  	.catch(function(error2){
@@ -99,6 +96,29 @@ $(function(){
 			console.log(error1);
 		});
 	});
+	*/
+	//--- getBatches ----------------------------------------------------------------------
+	const getBatches = function(){
+		//console.log('getBatches ');
+		
+		const msg = 'Info: Please wait, getting data.';
+		showInfoToUser(msg);
+
+		fetch(requestURL_getBatchNames).then(function(resultData){
+			resultData.json()
+		  	.then(function(rData){
+		  		renderAllBatchNames(rData);
+		  	})
+		  	.catch(function(error2){
+		  		console.log('JSON Error');
+		  		console.log(error2);
+		  	});
+		}).catch(function(error1){
+			console.log('getAllBatchNames: ERROR :--------------');
+			console.log(error1);
+		});
+	}
+	//--- getBatches / ----------------------------------------------------------------------
 
 	const renderAllBatchNames = function(aNames){
 		console.log('renderAllBatchNames');
@@ -117,57 +137,85 @@ $(function(){
 		$('#id_all_batches').html(allViewLi);
 		//
 		$('#id_all_batches').show();
+		//
+		$('#div_info').hide();
 	}
 	//---------------------------------------------------------------------
 	// Add Person to Batch
 	$('#btn_add_person').on('click',function(){
-		console.log('Add Person');
+		//console.log('Add Person');
 		//console.log('AddPerson',selectedBatchName,',',selectedBatchId);
-		
-		var sName = $('#txt_person_name').val();
-		var sNumber = $('#txt_person_phone').val();
-		var tNow = new Date();
-		var sTimeNow = tNow.getHours()+':'+tNow.getMinutes()+':'+tNow.getSeconds()+':'+tNow.getMilliseconds();
 
-		//console.log(sTimeNow+': '+sName+' / '+sNumber);
-		//console.log(selectedBatchName,selectedBatchId);
-		//
-		var jsonData = {
-			"calledOnTime": sTimeNow,
-			"personName": sName,
-			"phone":sNumber,
-			"batch":selectedBatchName,
-			"batchId":selectedBatchId,
-			"group":selectedGroupName
-		};
-		var jData = JSON.stringify(jsonData);
-		//console.log(jsonData);
-		//
+		if(addButtonIsDisabled){
+			// Do Nothing
+			console.log('AddPerson : Disabled');
 
-		var fetchData = {
-			  	method:'POST',
-			  	body: jData,
-			  	mode: 'cors',
-			  	headers:new Headers({
-			  		'Content-Type': 'application/json'
-			  	})
-			  };
-		fetch(requestURL_addPerson,fetchData).then(function(resultData){
-			console.log('------- RESULT --------');
-		  console.log(resultData);
-		  resultData.json()
-		  	.then(function(rData){
-		  		console.log(rData);
-		  	})
-		  	.catch(function(error1){
-		  		console.log('JSON Error');
-		  		console.log(error1);
-		  	});
-		}).catch(function(){
-			console.log('------ ERROR ----------');
-	  	console.log(error);
-	  	console.log('------ ERROR -------- /');
-		});
+			showInfoToUser('Info: You <strong>can not</strong> just, keep on adding. You have to wait.');
+
+		}else{
+			// Act
+			console.log('Add Person');
+			addButtonIsDisabled = true;
+			
+			
+			var sName = $('#txt_person_name').val();
+			var sNumber = $('#txt_person_phone').val();
+			var tNow = new Date();
+			var sTimeNow = tNow.getHours()+':'+tNow.getMinutes()+':'+tNow.getSeconds()+':'+tNow.getMilliseconds();
+
+			console.log(sTimeNow+': '+sName+' / '+sNumber);
+			//console.log(selectedBatchName,selectedBatchId);
+			// Check for empty
+			if(sName != '' && sNumber != ''){
+				// show info. Processing.
+				showInfoToUser('Please wait. The person is getting added to the Batch.');
+				//
+				var jsonData = {
+							"calledOnTime": sTimeNow,
+							"personName": sName,
+							"phone":sNumber,
+							"batch":selectedBatchName,
+							"batchId":selectedBatchId,
+							"group":selectedGroupName
+						};
+				var jData = JSON.stringify(jsonData);
+				var fetchData = {
+					  	method:'POST',
+					  	body: jData,
+					  	mode: 'cors',
+					  	headers:new Headers({
+					  		'Content-Type': 'application/json'
+					  	})
+						};
+				//
+				fetch(requestURL_addPerson,fetchData).then(function(resultData){
+					console.log('------- RESULT --------');
+				  console.log(resultData);
+				  resultData.json()
+				  	.then(function(rData){
+				  		console.log(rData);
+				  		//
+				  		addButtonIsDisabled = false;
+				  		//$('#div_info').hide();
+				  		showInfoToUser(rData.result);
+				  	})
+				  	.catch(function(error1){
+				  		console.log('JSON Error');
+				  		console.log(error1);
+				  	});
+				}).catch(function(){
+					console.log('------ ERROR ----------');
+			  	console.log(error);
+			  	console.log('------ ERROR -------- /');
+				});
+				//
+			}else{
+				// Show info. Can not be empty
+				showInfoToUser('Name or Phone <strong>can not</strong> be empty!')
+			}
+		}// if /END
+
+		return false;
 	});// Click Handler /END
 	//
 
@@ -175,5 +223,6 @@ $(function(){
 		$('#div_info').html(message);
 		$('#div_info').show();
 	}
-
+	//
+	getBatches();
 });
